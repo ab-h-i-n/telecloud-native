5da5fb14f373b3d15a9265e0e9efa6ee7dff8170
ab-h-i-n <abhinshelly@gmail.com>
2024-06-19 20:28:52 +0530

    signin page started

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,12 +1,22 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from "react-native";
+import React from "react";
+import centerImage from "../../assets/images/auth_center.png";
 
 const SignIn = () => {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>SignIn</Text>
-    </View>
-  )
-}
+    <View className="h-full bg-primary px-[19px] py-[74px] items-center">
+      {/* center image  */}
+      <Image source={centerImage} className="h-[289px] w-[309px]" />
 
-export default SignIn
+      {/* welcome text  */}
+      <Text className="text-white font-pextrabold text-[32px] text-center">
+        Welcome to, TeleCloud
+      </Text>
+
+      {/* input  box  */}
+
+    </View>
+  );
+};
+
+export default SignIn;
